9d42001b07a9190f065e89bbf7bad83218a0f99c
Toni Panacek <toni.panacek@gmail.com>
2017-11-15 12:16:36 +0100

    restaurant and review models for yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(
+  name: "Sukiyabashi Jiro",
+  address: "4 Chome-2-15 Ginza, Chuo, Tokyo, Japan",
+  phone_number: "+81 3-3535-3600",
+  category: "japanese"
+)
+Restaurant.create(
+  name: "Osteria Francescana",
+  address: "Via Stella, 22, 41121 Modena, Italy",
+  phone_number: "+39 059 223912",
+  category: "italian"
+)
+Restaurant.create(
+  name: "Silk Road",
+  address: "49 Camberwell Church St, London, UK",
+  phone_number: "+44 20 7703 4832",
+  category: "chinese"
+)
+Restaurant.create(
+  name: "Bon-Bon",
+  address: "Avenue de Tervueren 453, 1150 Bruxelles, Belgium",
+  phone_number: "+32 2 346 66 15",
+  category: "belgian"
+)
+Restaurant.create(
+  name: "Lilette",
+  address: "3637 Magazine St, New Orleans, USA",
+  phone_number: "+1 504-895-1636",
+  category: "french"
+)
